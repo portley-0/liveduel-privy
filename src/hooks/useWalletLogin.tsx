fd91510cda8599db480a6f833f0b7acf5386a17c
portley-0 <oisinportley@protonmail.com>
2025-04-14 15:31:30 +0100

    Remove unauthenticated requirement

--- src/hooks/useWalletLogin.tsx
+++ src/hooks/useWalletLogin.tsx
@@ -1,11 +1,11 @@
 import { usePrivy, useLogin } from "@privy-io/react-auth";
 
 export function useWalletLogin() {
-  const { ready, authenticated } = usePrivy();
+  const { ready } = usePrivy();
   const { login } = useLogin();
 
   const walletLogin = () => {
-    if (!ready || authenticated) return;
+    if (!ready ) return;
     login({
       loginMethods: ["wallet"],
       walletChainType: "ethereum-only",
